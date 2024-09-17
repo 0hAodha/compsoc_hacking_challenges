4a3fd75e49dbfb1399888d25ec089b736a7daa7f
Andrew <andreasohaodha@proton.me>
2024-09-17 18:10:24 +0100

    [squeal]: print challenge information

--- squeal/squeal.sh
+++ squeal/squeal.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Script that accepts the name of an artist and checks if they are currently in our Billboard Top 10 database
 
+echo "This script accepts the name of an artist and checks if they are currently in the Billboard Top 10 table in our database, e.g. Sabrina Carpenter"
+echo "The database also contains a 'flags' table, but you shouldn't be able to access that..."
+
 while true; do
     printf "Enter an artist's name: "
     read input 
