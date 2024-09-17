9430e6ca8dc0a28b6e9bd840eca51f54e49a6b5f
Andrew <andreasohaodha@proton.me>
2024-09-17 18:06:06 +0100

    [approved]: print challenge information

--- approved/approved.pl
+++ approved/approved.pl
@@ -4,6 +4,13 @@
 
 my @approved_commands = ("ls", "pwd", "whoami");
 
+print("Read the contents of flag.txt. You can only use commands in the approved list.
+Hint: You can supply whatever parameters you like to the command!
+");
+
+print("Approved commands: ");
+print("\n - $_") foreach (@approved_commands);
+
 while (1) {
     printf("\n> ");
     my $command = <STDIN>;
